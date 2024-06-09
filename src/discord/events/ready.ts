289cff6af8e8478e031cd64b5745d9eab068a186
Lucid <nohack-@naver.com>
2024-06-10 00:03:39 +0900

    update: 불필요한 채널 메시지 제거

--- src/discord/events/ready.ts
+++ src/discord/events/ready.ts
@@ -3,6 +3,6 @@ import { Client } from 'discord.js';
 export const ready = (client: Client) => {
   const channel = client.guilds.cache.first().systemChannel;
 
-  channel.send('백해의 조화와 약속을 수호하는 땅지기 등장이오!');
+  // channel.send('백해의 조화와 약속을 수호하는 땅지기 등장이오!');
   console.log(`👾 [${client.user.tag}] 실행 완료`);
 };
